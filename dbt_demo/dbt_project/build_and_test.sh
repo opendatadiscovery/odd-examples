f73a8633a5ddcab65c7747c8816e87d8a93fe45f
Pavel Mackarichev <vixtir90@gmail.com>
2023-09-13 18:57:42 +0300

    chore: bump dbt

--- dbt_demo/dbt_project/build_and_test.sh
+++ dbt_demo/dbt_project/build_and_test.sh
@@ -3,4 +3,5 @@ dbt build --profiles-dir profiles
 UNIQ_NAME="dbt_demo_$(uuidgen)"
 export ODD_COLLECTOR_TOKEN="$(odd tokens create $UNIQ_NAME)"
 echo "ODD_COLLECTOR_TOKEN=$ODD_COLLECTOR_TOKEN"
-odd dbt test --profiles-dir profiles -t $ODD_COLLECTOR_TOKEN -h $ODD_PLATFORM_HOST
+odd dbt create-datasource -t $ODD_COLLECTOR_TOKEN -h $ODD_PLATFORM_HOST --name=dbt_demo --dbt-host=localhost
+odd dbt test --profiles-dir profiles -t $ODD_COLLECTOR_TOKEN -h $ODD_PLATFORM_HOST -oddrn //dbt/host/localhost

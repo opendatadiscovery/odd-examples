eb9e6d696700ca3ca8125781bab181d2f2117814
Pavel Mackarichev <vixtir90@gmail.com>
2023-09-18 19:14:01 +0300

    feat: update dbt

--- dbt_demo/dbt_project/build_and_test.sh
+++ dbt_demo/dbt_project/build_and_test.sh
@@ -1,7 +1,10 @@
 dbt build --profiles-dir profiles
+dbt run --profiles-dir profiles
+dbt test --profiles-dir profiles
 
 UNIQ_NAME="dbt_demo_$(uuidgen)"
 export ODD_COLLECTOR_TOKEN="$(odd tokens create $UNIQ_NAME)"
 echo "ODD_COLLECTOR_TOKEN=$ODD_COLLECTOR_TOKEN"
 odd dbt create-datasource -t $ODD_COLLECTOR_TOKEN -h $ODD_PLATFORM_HOST --name=dbt_demo --dbt-host=localhost
-odd dbt test --profiles-dir profiles -t $ODD_COLLECTOR_TOKEN -h $ODD_PLATFORM_HOST -oddrn //dbt/host/localhost
+odd dbt ingest-test --profiles-dir profiles -t $ODD_COLLECTOR_TOKEN -h $ODD_PLATFORM_HOST -oddrn //dbt/host/localhost
+odd dbt ingest-lineage  --profiles-dir profiles -t $ODD_COLLECTOR_TOKEN -h $ODD_PLATFORM_HOST -oddrn //dbt/host/localhost
